68be7ca54837a342a9ef327e077f0f2c16216ca3
Matteo0421 <matteo.carola99@gmail.com>
2024-03-06 16:30:15 +0100

    [add]function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
+
 const images = [
   {
       url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
       title: 'Svezia',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
   },
-
   {
       url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
       title: 'Perù',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
   },
-
   {
       url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
       title: 'Chile',
@@ -29,19 +28,38 @@ const images = [
 ];
 
 const output = document.querySelector('.output');
+const thumbnails = document.querySelectorAll('.my-thumbnail');
+let currentImageIndex = 0;
 
 
-for(let image of images){
-  console.log(image);
- 
 
-  output.innerHTML += `
-  <div class="my-carousel-item active">
-  <img class="img-fluid" src="${image.url}"  picture">
-  <div class="item-description px-3">
-      <h2>${image.title}</h2>
-      <p>${image.description}</p>
-  </div>
-</div>
-  `
+// Mostra l'immagine iniziale
+showCurrentImage();
+
+// Gestione del clic sul pulsante "Prev"
+document.querySelector('.my-previous').addEventListener('click', () => {
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  showCurrentImage();
+});
+
+// Gestione del clic sul pulsante "Next"
+document.querySelector('.my-next').addEventListener('click', () => {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  showCurrentImage();
+});
+
+
+
+// Funzione per visualizzare l'immagine corrente
+function showCurrentImage() {
+  const currentImage = images[currentImageIndex];
+  output.innerHTML = `
+      <div class="my-carousel-item active">
+          <img class="img-fluid" src="${currentImage.url}" alt="${currentImage.title} picture">
+          <div class="item-description px-3">
+              <h2>${currentImage.title}</h2>
+              <p>${currentImage.description}</p>
+          </div>
+      </div>
+  `;
 }
